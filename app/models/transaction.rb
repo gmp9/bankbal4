3ad4ade415dad9d1d9ac199d65908425858f10d1
gmp9 <gmpretto@sbcglobal.net>
2016-10-11 05:42:00 +0000

    added exception handling to model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,16 @@ class Transaction < ActiveRecord::Base
     
     def calc(item1,item2,item3)
         item1 + item2 - item3
+        #in case a number is accidentally left out(nil), raise an exception & return a large #.
+        rescue
+          55555.55
     end
     
     def sumwdl(num1,num2,num3)
         num1 + num2 + num3
+        #in case a number is accidentally left out(nil), raise an exception & return a large #.
+        rescue
+          99999.99
     end
     
 end
